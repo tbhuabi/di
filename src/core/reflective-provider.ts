165bea0dbef9be741ac8d69593f3c21f51144e05
tanbo <464328895@qq.com>
2021-03-23 20:30:59 +0800

    feat:优化类型及错误提示

--- src/core/reflective-provider.ts
+++ src/core/reflective-provider.ts
@@ -156,7 +156,7 @@ function normalizeDeps(provide: any, deps: any[]): ReflectiveDependency[] {
     }
     if (typeof r.injectKey === 'undefined') {
       throw new Error(`the ${index} th dependent parameter type of \`${stringify(provide)}\` was not obtained,
-if the dependency is declared later, you can refer to it using forwardref\`constructor(@Inject(forwardRef(() => [Type])) paramName: [Type]) {}\``);
+if the dependency is declared later, you can refer to it using \`constructor(@Inject(forwardRef(() => [Type])) paramName: [Type]) {}\``);
     }
     return r;
   })
